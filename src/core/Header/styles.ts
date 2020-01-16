1d598fb8cd897429c3a2ce77bffc9448694d21f4
kavk <knutandrek@gmail.com>
2020-01-16 13:16:16 +0100

    Fixed build

--- src/core/Header/styles.ts
+++ src/core/Header/styles.ts
@@ -1,29 +1,30 @@
 import themeColors from '../../globals/styles/colors';
+import { css } from '@emotion/core';
 
 
 const styles = {
-    default: {
+    default: css({
         fontWeight: 'bold',
         margin: '0'
-    }, 
-    h1: {
+    }), 
+    h1: css({
         fontSize: '40px',
         fontWeight: 'bold',
         color: themeColors.defaultText,
         margin: '0'
-    },
-    h2: {
+    }),
+    h2: css({
         fontSize: '32px',
-    },
-    h3: {
+    }),
+    h3: css({
         fontSize: '24px',
-    },
-    h4: {
+    }),
+    h4: css({
         fontSize: '22px',
-    },
-    h5: {
+    }),
+    h5: css({
         fontSize: '18px',
-    }
+    })
 }
 
 
