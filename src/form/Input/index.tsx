7328bf8bcec36ebf9d7a0f633bec960cf2ba71ca
kavk <knutandrek@gmail.com>
2020-01-16 13:25:24 +0100

    Added storybook-static to gitignore, fixed empty components

---
+++ src/form/Input/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from '@emotion/styled/macro'
+
+const Input: React.FC<IProps> = styled.input`
+    borderRadius: 3px
+`
+
+interface IProps {
+}
+
+export default Input;
---
+++ src/form/Select/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from '@emotion/styled/macro'
+
+const Select: React.FC<IProps> = styled.select`
+    borderRadius: 3px
+`
+
+interface IProps {
+}
+
+export default Select;
