b014121881e34dfdf1a5e644c3749f3e8cf4c3c4
Kristian L. Grønvold <klg@bbl.no>
2020-01-30 09:56:44 +0100

    added input field component

--- src/core/Input/index.tsx
+++ src/core/Input/index.tsx
@@ -4,23 +4,49 @@ import { jsx } from '@emotion/core'
 import { styles } from './styles'
 
 interface IProps {
-  type?: 'text' | 'search' | 'number'
-  label: string
+  type?: 'text' | 'search' | 'number' | 'password'
+  label?: string
+  id?: string
+  highlight?: boolean
+  focus?: boolean
+  placeholder?: string
+  value?: string
+  disabled?: boolean
 }
 
 const Input: React.FC<IProps> = props => {
-  const type: 'text' | 'search' | 'number' = props.type ? props.type : 'text'
+  const type: 'text' | 'search' | 'number' | 'password' = props.type
+    ? props.type
+    : 'text'
+  const placeholder: string = props.placeholder ? props.placeholder : ''
+  const label: string = props.label ? props.label : ''
+  const value: string = props.value ? props.value : ''
 
   return (
-    <input type={type} css={[styles.default]}>
-      {React.Children.map(props.children, child => {
-        if (!child) {
-          return null
-        }
+    <label css={[styles.default, props.highlight && styles.highligted]}>
+      <span css={props.focus && styles.focusedLabel}>{label}</span>
+      <input
+        type={type}
+        placeholder={placeholder}
+        defaultValue={value}
+        disabled={props.disabled}
+        css={[
+          props.type === 'text' && styles.text,
+          props.type === 'search' && styles.search,
+          props.type === 'password' && styles.password,
+          props.focus && styles.focused,
+          props.disabled && styles.disabled,
+        ]}
+      >
+        {React.Children.map(props.children, child => {
+          if (!child) {
+            return null
+          }
 
-        return child
-      })}
-    </input>
+          return child
+        })}
+      </input>
+    </label>
   )
 }
 
--- src/core/Input/styles.ts
+++ src/core/Input/styles.ts
@@ -3,11 +3,63 @@ import { css } from '@emotion/core'
 
 const styles = {
   default: css({
-    boxSizing: 'border-box',
-    height: '36px',
-    width: '340px',
-    border: '2px solid ' + themeColors.primary,
+    display: 'grid',
+    ':focus-within span': {
+      color: themeColors.primary,
+    },
+    span: {
+      minHeight: '25px',
+      fontWeight: 'bold',
+    },
+    input: {
+      boxSizing: 'border-box',
+      height: '36px',
+      width: '340px',
+      fontSize: '16px',
+      outline: 'none',
+      ':focus': {
+        borderColor: themeColors.primary,
+      },
+      ':disabled': {
+        backgroundColor: themeColors.disabled,
+      },
+    },
+    'input:focus + label': {
+      color: themeColors.primary,
+    },
+  }),
+  highligted: css({
+    fontSize: '20px',
+    input: {
+      height: '50px',
+      fontSize: '18px',
+    },
+  }),
+  text: css({
+    border: '2px solid ' + themeColors.border,
     borderRadius: '3px',
+    paddingLeft: '10px',
+  }),
+  password: css({
+    border: '2px solid ' + themeColors.border,
+    borderRadius: '3px',
+    paddingLeft: '10px',
+  }),
+  search: css({
+    border: '0',
+    borderBottom: '2px solid ' + themeColors.border,
+    paddingLeft: '0',
+  }),
+  disabled: css({
+    cursor: 'not-allowed',
+    backgroundColor: themeColors.disabled,
+    borderColor: themeColors.disabled,
+  }),
+  focused: css({
+    borderColor: themeColors.primary,
+  }),
+  focusedLabel: css({
+    color: themeColors.primary,
   }),
 }
 
