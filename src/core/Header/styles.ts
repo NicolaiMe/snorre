89b037c25413a44bda73980f76a1af1a91b36e38
Knut-André <kak@bbl.no>
2020-02-04 09:28:15 +0100

    More styles

--- src/core/Header/styles.ts
+++ src/core/Header/styles.ts
@@ -1,40 +1,41 @@
-import themeColors from '../../globals/styles/colors'
 import { css } from '@emotion/core'
 import bp from '../../globals/styles/breakpoints'
 
 const styles = {
-  default: css({
-    fontWeight: 'bold',
-    margin: '0',
-  }),
+  default: css`
+    font-weight: bold;
+    margin: 0;
+  `,
   h1: css`
-    font-size: '40px',
-    font-weight: 'bold',
-    color: ${themeColors.defaultText},
-    margin: '0',
-    @media screen and (min-width: ${bp.small}) {
-        font-size: '31px'
-
+    font-size: 40px;
+    font-weight: bold;
+    margin: 0;
+    @media screen and (max-width: ${bp.small}) {
+      font-size: 31px;
     }
   `,
   h2: css`
-    font-size: '32px', @media screen and (min-width: ${bp.small}) {
-      font-size: '22px';
+    font-size: 32px;
+    @media screen and (max-width: ${bp.small}) {
+      font-size: 22px;
     }
   `,
   h3: css`
-    font-size: '24px', @media screen and (min-width: ${bp.small}) {
-      font-size: '20px';
+    font-size: 24px;
+    @media screen and (max-width: ${bp.small}) {
+      font-size: 20px;
     }
   `,
   h4: css`
-    font-size: '22px', @media screen and (min-width: ${bp.small}) {
-      fontsize: '18px';
+    font-size: 22px;
+    @media screen and (max-width: ${bp.small}) {
+      font-size: 18px;
     }
   `,
   h5: css`
-    font-size: '18px', @media screen and (min-width: ${bp.small}) {
-      font-size: '16px';
+    font-size: 18px;
+    @media screen and (max-width: ${bp.small}) {
+      font-size: 16px;
     }
   `,
 }
